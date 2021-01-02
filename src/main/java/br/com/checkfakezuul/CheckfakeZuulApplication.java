ddfe93daadb7f50ae121765b8dd700714497d9ce
Caue <cbeloni@gmail.com>
2021-01-02 10:57:41 -0300

    first commit

---
+++ src/main/java/br/com/checkfakezuul/CheckfakeZuulApplication.java
@@ -0,0 +1,17 @@
+package br.com.checkfakezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class CheckfakeZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckfakeZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/checkfakezuul/CheckfakeZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.checkfakezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckfakeZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
